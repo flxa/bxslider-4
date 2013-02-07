7477de759429859153d18d5b2e72cc71c0a763d6
Nicholas Fletcher <nick.flxa@gmail.com>
2013-02-08 03:51:02 +1100

    Wordpress plugin
    By creating a category in wordpress and creating some posts with images
    or iframes for video you can then add `<?php echo bxslider(
    'front-page-slider' ); ?>` to your template and will render a
    responsive slider

---
+++ bxslider.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * @package bxslider
+ * @version 1.0
+ */
+/*
+Plugin Name: bxslider
+Plugin URI: http://flxa.co/
+Description: A responsive carousel slider written in CSS and javascript.
+Version: 1.0
+Author: flxa
+Author URI: http://flxa.co
+License: GPL2
+*/
+
+/*  Copyright 2013  flxa  (email : hi@flxa.co)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+function bxslider_get_posts($cat=null) {
+
+    $args = array(
+        'category_name'   => $cat,
+        'orderby'         => 'post_date',
+        'order'           => 'DESC',
+        'post_type'       => 'post',
+        'post_status'     => 'publish',
+        'suppress_filters' => true ); 
+
+    $posts_array = get_posts( $args ); 
+
+    foreach ($posts_array as $key => $post) {
+        $content .= '<li>'.$post->post_content.'</li>';
+    }
+
+    return $content;
+}
+
+function bxslider($cat) {
+    $output = bxslider_get_posts($cat);
+    $html = "<ul class='bxslider'>$output</ul>";
+    return $html;
+}
+
+add_action( 'admin_notices', 'bxslider' );
+
+function bxslider_css() {
+    wp_register_style( 'bxslider-style', plugins_url('css/jquery.bxslider.css', __FILE__) );
+    wp_enqueue_style( 'bxslider-style' );
+}
+add_action( 'wp_enqueue_scripts', 'bxslider_css' );
+add_action( 'admin_head', 'bxslider_css' );
+
+function bxslider_js()  {
+    wp_enqueue_script( 'fitvidsjs', plugins_url('js/plugins/jquery.fitvids.js', __FILE__), array('jquery') );
+    wp_enqueue_script( 'bxsliderjs', plugins_url('js/jquery.bxslider.min.js', __FILE__), array('jquery') );
+}
+add_action( 'wp_enqueue_scripts', 'bxslider_js' );
+add_action( 'admin_head', 'bxslider_js' );
+?>
